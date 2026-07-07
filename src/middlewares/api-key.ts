7e4a8f825eca4cdebe7bf665b2ad31dd09658cf2
Juan Marquina <opgr61@atu.gob.pe>
2026-07-06 22:07:18 -0500

    se elimina  mensaje de consola

--- src/middlewares/api-key.ts
+++ src/middlewares/api-key.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction } from "express";
 
 export const apiKeyAuth = (req: Request, res: Response, next: NextFunction) => {
     const apiKey = req.header("x-api-key"); // el cliente manda: x-api-key: <API_KEY>
-    console.log('API KEY enviado:', apiKey);
     if (!apiKey || apiKey !== process.env.API_KEY) {
         return res.status(401).json({ message: "No autorizado" });
     }
